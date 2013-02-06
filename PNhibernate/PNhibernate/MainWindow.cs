7825c690064bb25e41f9d17a1b58cc6a40bdc7a9
Your NametoniAlvarez <xacon_93@hotmail.com>
2013-02-06 08:23:44 +0100

    subiendo PNHibernate con añadidos

--- PNhibernate/PNhibernate/MainWindow.cs
+++ PNhibernate/PNhibernate/MainWindow.cs
@@ -3,28 +3,32 @@ using Gtk;
 
 public partial class MainWindow: Gtk.Window
 {	
-	public MainWindow (): base (Gtk.WindowType.Toplevel)
+public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		
-		Configuration configuration=new Configuration();
-		configuration.Configure();
-		
-		configuration.AddAssembly(typeof(Categoria).Assembly);
-		//new SchemaExport(configurations).Execute(true, false, false);
-		
-		ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-		
-		ISession session = sessionFactory.OpenSession();
-		Categoria categoria = (Categoria)session.Load(typeof(categoria),2L);
-		Console.WriteLine ("Categoria Id={0} Nombre={1}", categoria.Id, categoria.Nombre);
-		categoria.Nombre = DataTime.Now.ToString();
-		session.SaveOrUpdate(categoria);
 
-		session.close();
-		sessionFactory.close();
+		Configuration configuration = new Configuration();
+		configuration.Configure ();
+		configuration.SetProperty(NHibernate.Cfg.Environment.Hbm2ddlKeyWords, "none");
+		configuration.AddAssembly(typeof(Categoria).Assembly);
+
+		//new SchemaExport(configuration).Execute(true, false, false);
+
+		ISessionFactory sessionFactory = configuration.BuildSessionFactory ();
+
+		ISession session = sessionFactory.OpenSession();
+		Categoria categoria = (Categoria)session.Load(typeof(Categoria), 2L);
+		Console.WriteLine("Categoria Id={0} Nombre={1}", categoria.Id, categoria.Nombre);
+		categoria.Nombre = DateTime.Now.ToString ();
+		session.SaveOrUpdate (categoria);
+		session.Flush ();
+		session.Close ();
+
+		sessionFactory.Close ();
+
+
 	}
-	
+
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
